e9ddc211bc21a808e034596265501a0b2e70c23f
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2018-03-13 10:23:36 +0100

    Fixed inspection errors, removed content-type analysis for getData (need a better xml deserializer)

--- src/Denner/Client/Response/BaseResponse.php
+++ src/Denner/Client/Response/BaseResponse.php
@@ -50,31 +50,17 @@ abstract class BaseResponse implements
     protected function getData()
     {
         try {
-
-            switch ($this->getHttpResponse()->getHeader('Content-Type')) {
-                case 'application/xml; charset=utf-8':
-                case 'application/xml':
-                    /**
-                     * @todo With this we loose all xml attributes if any
-                     * @param \SimpleXMLElement $xmlObject
-                     * @param array $out
-                     * @return array
-                     */
-                    $xml2array = function($xmlObject, $out = array()) use (&$xml2array) {
-                        foreach ((array) $xmlObject as $index => $node)
-                            $out[$index] = (is_object($node) || is_array($node)) ? $xml2array($node) : $node;
-
-                        return $out;
-                    };
-
-                    $data = $xml2array($this->getHttpResponse()->xml());
-                    break;
-                case 'application/json; charset=utf-8':
-                case 'application/json':
-                default:
+//            switch ($this->getHttpResponse()->getHeader('Content-Type')) {
+//                case 'application/xml; charset=utf-8':
+//                case 'application/xml':
+//                    $data = $this->xml2array($this->getHttpResponse()->xml());
+//                    break;
+//                case 'application/json; charset=utf-8':
+//                case 'application/json':
+//                default:
                     $data = $this->getHttpResponse()->json() ?: array();
-                    break;
-            }
+//                    break;
+//            }
         } catch (GuzzleHttpException\ParseException $e) {
             throw new Exception\RuntimeException(
                 sprintf('Parse exception requesting \'%s\'', $e->getResponse()->getEffectiveUrl()),
@@ -93,4 +79,19 @@ abstract class BaseResponse implements
     {
         return $this->getData();
     }
+
+//    /**
+//     * @todo With this we loose all xml attributes if any and all listings are incorrectly handled (numeric keys)
+//     * @param \SimpleXMLElement|array $xmlObject
+//     * @param array $out
+//     * @return array
+//     */
+//    private function xml2array($xmlObject, $out = array())
+//    {
+//        foreach ((array) $xmlObject as $index => $node) {
+//            $out[$index] = (is_object($node) || is_array($node)) ? $this->xml2array($node) : $node;
+//        }
+//
+//        return $out;
+//    }
 }
