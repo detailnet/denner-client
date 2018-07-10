b3e60f21ef080c63dc8692c5bd98d5c330458946
Ivan Wolf <iwolf@detailag.ch>
2018-07-10 16:00:55 +0200

    Documented missing methods

--- src/AdvertisingClient.php
+++ src/AdvertisingClient.php
@@ -9,6 +9,8 @@ use Denner\Client\Response;
  *
  * @method Response\ListResponse listPrintPublications(array $params = [])
  * @method Response\ResourceResponse fetchPrintPublication(array $params = [])
+ * @method Response\ListResponse listPrintPublicationArticles(array $params = [])
+ * @method Response\ListResponse listPrintPublicationLinks(array $params = [])
  */
 class AdvertisingClient extends DennerClient
 {
