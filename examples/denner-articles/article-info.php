f4fdd78df744a3af9a34352eb097c9443728bc23
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2019-10-22 16:30:12 +0200

    Fixed texts

--- examples/denner-articles/article-info.php
+++ examples/denner-articles/article-info.php
@@ -4,19 +4,19 @@ use Denner\Client\ArticlesClient;
 
 $config = require realpath(__DIR__ . '/../bootstrap.php');
 
-$advertisedArticleId = @$_GET['article_id'] ?: '051583';
+$articleId = @$_GET['article_id'] ?: '051583';
 $quantity = @$_GET['quantity'] ?: 6;
 $wineYear = @$_GET['wine_year'] ?: 2016;
 
-if (!$advertisedArticleId) {
-    throw new RuntimeException('Missing or invalid parameter "advertised_article_id"');
+if (!$articleId) {
+    throw new RuntimeException('Missing or invalid parameter "article_id"');
 }
 
 $client = ArticlesClient::factory($config);
 
 $response = $client->fetchArticle(
     [
-        'article_id' => $advertisedArticleId,
+        'article_id' => $articleId,
         'quantity' => (int) $quantity,
         'wine-year' => (int) $wineYear,
     ]
