ec561a6419513493019db9e5755d18e92af011ab
Curzio Della Santa <curzio.della.santa@detailnet.ch>
2026-02-09 12:45:59 +0100

    Permit trade item type code as article quantity

--- src/ServiceDescription/Articles.php
+++ src/ServiceDescription/Articles.php
@@ -90,13 +90,13 @@ return [
                     'required' => true,
                 ],
                 'quantity' => [
-                    'description' => 'Quantity (for texts and price)',
+                    'description' => 'Quantity (for texts and price); Trade item type code is accepted too.',
                     // Undocumented feature:
                     //   When the quantity is negative, the positive value is for prices, but default texts have to be returned.
                     //   Ref [CR-210823](https://3.basecamp.com/3648441/buckets/7231296/todos/4077892797)
                     'location' => 'query',
                     'required' => false,
-                    'type' => 'integer',
+                    'type' => ['integer', 'string'],
                 ],
                 'price-selection' => [
                     '$ref' => 'PriceSelectionParam',
